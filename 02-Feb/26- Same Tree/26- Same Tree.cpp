57d7ee336156667c6e8e2c4548bf3060f418d7b6
Mazen Ghanayem <mznghanayem@gmail.com>
2024-02-26 02:21:39 +0200

    Add 26- Same Tree

---
+++ 02-Feb/26- Same Tree/26- Same Tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+        if (p == NULL && q == NULL)
+            return 1;
+        if (p == NULL || q == NULL)
+            return 0;
+        if (p->val != q->val)
+            return 0;
+        return isSameTree(p->left, q->left) & isSameTree(p->right, q->right);
+    }
+};
