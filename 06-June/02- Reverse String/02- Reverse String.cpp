941c75c80afd844c248325a8bd096961104ebfe9
Mazen Ghanayem <mznghanayem@gmail.com>
2024-06-02 20:07:40 +0300

    Add 2- Reverse String

---
+++ 06-June/02- Reverse String/02- Reverse String.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    void reverseString(vector<char> &s)
+    {
+        ::reverse(s.begin(), s.end());
+    }
+};
